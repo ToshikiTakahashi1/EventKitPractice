51db6cdac36fc05d72868f20eb6b7c11e3eb0914
Toshiki Takahashi <t.toshiki.app@gmail.com>
2025-04-19 20:45:18 +0900

    アクセス権限の取得とイベントインスタンスを反映

--- EventKitPractice/EventKitPracticeApp.swift
+++ EventKitPractice/EventKitPracticeApp.swift
@@ -1,7 +1,19 @@
 import SwiftUI
+import EventKit
 
 @main
 struct EventKitPracticeApp: App {
+    
+    init() {
+        Task {
+            do {
+                try await EKEventStore().requestFullAccessToEvents()
+            } catch {
+                // TODO: Error Handling
+            }
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
--- EventKitPractice/EventView.swift
+++ EventKitPractice/EventView.swift
@@ -1,11 +1,15 @@
 import UIKit
 import SwiftUI
+import EventKit
 import EventKitUI
 
 struct EventView: UIViewControllerRepresentable {
     
     func makeUIViewController(context: Context) -> some UIViewController {
-        return EKEventViewController()
+        let event = EKEvent(eventStore: EKEventStore())
+        let eventViewController = EKEventViewController()
+        eventViewController.event = event
+        return eventViewController
     }
     
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
